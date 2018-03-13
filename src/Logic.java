3f537d7862d8953cda628cb1e94e336def761636
Rigi <rrigi@contractor.ea.com>
2018-03-12 18:15:54 -0700

    Hi

---
+++ src/Logic.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class Logic {
+
+    public int bidNumber;
+    public int auctionHouse;
+    public int photoID;
+    public final static int BID_RANGE = 100000;
+    public final static int AUCTION_RANGE = 90;
+    public final static int PHOTO_RANGE = 4;
+
+    ArrayList urlList = new ArrayList();
+
+
+    //http://p3.aleado.com/pic?system=auto&date=2018-03-13&auct=8&bid=20085&number=1
+
+
+    public String incrementUrl(){
+
+            auctionHouse = 0;
+            bidNumber = 0;
+            photoID = 0;
+            String url = "lol";
+            boolean run = true;
+
+
+            while (run){
+                photoID++;
+                if(photoID == PHOTO_RANGE){
+                    bidNumber++;
+                    photoID = 0;
+                    if(bidNumber ==BID_RANGE){
+                        auctionHouse++;
+                        bidNumber = 0;
+                        if(auctionHouse == AUCTION_RANGE){
+                        run = false;
+                    }}
+                }
+                url = "http://p3.aleado.com/pic?system=auto&date=2018-03-13&auct=" + auctionHouse +"&bid="+ bidNumber + "&number=" + photoID;
+                urlList.add(url);
+            }
+        return url;
+    }
+}
+
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+
+    public static void main (String args[]){
+        Logic logic = new Logic();
+        logic.incrementUrl();
+
+    }
+
+}
